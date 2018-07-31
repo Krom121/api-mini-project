6b12a1d1784e0a36de8a077591c4bba787df3b8b
krom121 <stephenmacgregor9@gmail.com>
2018-07-31 18:03:21 +0100

    added structure to ajax-test added xhtml request xhr

---
+++ ajax-test/main.js
@@ -0,0 +1,13 @@
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200){
+        document.getElementById("data").innerHTML = this.responseText;
+}
+};
+
+/* Open the request to where data is*/
+
+xhr.open("GET", "https://swapi.co/api/");
+
+xhr.send();
