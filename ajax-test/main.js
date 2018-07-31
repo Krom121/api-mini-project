7e7a53e7b3d516d615a94c77e95295ce8d93d3fa
krom121 <stephenmacgregor9@gmail.com>
2018-07-31 18:29:01 +0100

    added functional

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,13 +1,24 @@
 var xhr = new XMLHttpRequest();
+var data;
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200){
-        document.getElementById("data").innerHTML = this.responseText;
-}
-};
 
-/* Open the request to where data is*/
-
+/* Open the request to where data is */
 xhr.open("GET", "https://swapi.co/api/");
 
 xhr.send();
+
+function setData(jsonData){
+    data = jsonData;
+    console.log(data);
+}
+
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200){
+       setData(JSON.parse(this.responseText));
+       
+}
+};
+
+
+
