f3dcc8765587ceff86447a284f1a69a25df14d84
krom121 <stephenmacgregor9@gmail.com>
2018-07-31 21:35:18 +0100

    timeout

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -9,16 +9,17 @@ xhr.send();
 
 function setData(jsonData){
     data = jsonData;
-    console.log(data);
+    
 }
 
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200){
-       setData(JSON.parse(this.responseText));
+       data = JSON.parse(this.responseText);
        
 }
 };
 
-
-
+setTimeout(function(){
+    console.log(data);
+}, 500);
